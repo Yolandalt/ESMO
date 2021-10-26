2007392863ba20afbb5ccf8116c768b4f6a67fc1
Yolandalt <44331294+Yolandalt@users.noreply.github.com>
2021-10-26 09:14:46 +0800

    Add files via upload

--- sko/operators/mutation.py
+++ sko/operators/mutation.py
@@ -79,28 +79,3 @@ def mutation_swap(self):
             self.Chrom[i] = swap(self.Chrom[i])
     return self.Chrom
 
-def mutation_Y(self, Pm=0.2):
-    """
-    :param population_Y: 经交叉后得到的种群
-    :param Pm: 变异概率默认是0.01
-    :return: 经变异操作后的新种群
-    """
-    # updatepopulation_Y = np.copy(self.Chrom)
-    m, n = self.Chrom.shape
-    # 计算需要变异的基因个数
-    gene_num = np.uint8(m * n * Pm)
-    # 将所有的基因按照序号进行10进制编码，则共有m*n个基因
-    # 随机抽取gene_num个基因进行基本位变异
-    mutationGeneIndex = random.sample(range(0, m * n), gene_num)
-    # 确定每个将要变异的基因在整个染色体中的基因座(即基因的具体位置)
-    for gene in mutationGeneIndex:
-        # 确定变异基因位于第几个染色体
-        chromosomeIndex = gene // n
-        # 确定变异基因位于当前染色体的第几个基因位
-        geneIndex = gene % n
-        # mutation
-        if self.Chrom[chromosomeIndex, geneIndex] == 0:
-            self.Chrom[chromosomeIndex, geneIndex] = 1
-        else:
-            self.Chrom[chromosomeIndex, geneIndex] = 0
-    return self.Chrom
